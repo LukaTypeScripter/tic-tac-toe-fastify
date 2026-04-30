b6da9703d5c6467bec1d858fffad72973e309eed
luka shinjikashvili <114741110+LukaTypeScripter@users.noreply.github.com>
2026-04-30 11:59:36 -0700

    Delete dist directory

--- dist/config/server.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const serverConfig: {
-    host: string;
-    port: number;
-};
-//# sourceMappingURL=server.d.ts.map
--- dist/types/web-socket-client.type.d.ts
+++
@@ -1,5 +0,0 @@
-export type WebSocketClient = {
-    readyState: number;
-    send(message: string): void;
-};
-//# sourceMappingURL=web-socket-client.type.d.ts.map
