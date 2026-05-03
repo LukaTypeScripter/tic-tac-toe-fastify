4803bc2b4d97f5d1ed67a592aaad880f49715d39
LukaTypeScripter <lukashinjikashvili85@gmail.com>
2026-05-03 22:41:42 +0400

    refactor(drizzle): remove unused imports for cleaner code

--- src/plugins/drizzle.ts
+++ src/plugins/drizzle.ts
@@ -3,8 +3,7 @@ import { drizzle } from "drizzle-orm/node-postgres";
 import type { NodePgDatabase } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 import * as schema from "../db/schema.js";
-import {existsSync} from "node:fs";
-import {loadEnvFile} from "node:process";
+
 
 
 
